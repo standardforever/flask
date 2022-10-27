0191fc32145cb65c99d6d599cd4b1f86a2f35c87
kusimosemiu@gmail.com <standard.forever123@gmail.com>
2022-10-27 10:21:58 -0400

    updated

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from flask import Flask
+from flask import jsonify
+
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return '({"SlackUsername": "","Backend": True,"Age": 21,"Bio": "I am passionate about tech"})'
+
+if __name__ == "__main__":
+    app.run(debug=False)
