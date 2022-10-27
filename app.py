dd7c2db767d7dd793f738e740d0089eebc28336b
kusimosemiu@gmail.com <standard.forever123@gmail.com>
2022-10-27 12:11:40 -0400

    updated

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return '({"SlackUsername": "","Backend": True,"Age": 21,"Bio": "I am passionate about tech"})'
+    return {"SlackUsername": "standardforever","Backend": True,"Age": 21 ,"Bio": "I am passionate about tech"}
 
 if __name__ == "__main__":
     app.run(debug=False)
