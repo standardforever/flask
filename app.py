d2a6a7aa9873acf5b1f8d52273e5cab8f013b765
standardforever <standard.forever123@gmail.com>
2022-10-28 06:53:01 +0100

    updated

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask
 from flask import jsonify
-
+from flask_cors import CORS
 
 app = Flask(__name__)
-
+CORS(app)
 @app.route('/')
 def hello_world():
     return {"slackUsername": "standardforever","backend": True,"age": 21 ,"bio": "I am passionate about tech"}
