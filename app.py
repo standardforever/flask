7dc7245cabd47b2eed88d7e52417ad03fdedfefa
kusimosemiu@gmail.com <standard.forever123@gmail.com>
2022-11-04 18:47:13 -0400

    updated

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask
 from flask import jsonify
 from flask import request
+from flask_cors import CORS
 
 app = Flask(__name__)
+
+CORS(app)
 
 """Stage 1 Task"""
 @app.route('/')
